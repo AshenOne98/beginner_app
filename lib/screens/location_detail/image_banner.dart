4e8f9ee2b3864f2811d9b9614d110ff57c118eea
Muhd Arif <codeprogram98@gmail.com>
2021-01-24 22:33:10 +0800

    starting template

---
+++ lib/screens/location_detail/image_banner.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ImageBanner extends StatelessWidget {
+  final String _assetPath;
+
+  ImageBanner(this._assetPath);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('h1'),
+    );
+  }
+}
